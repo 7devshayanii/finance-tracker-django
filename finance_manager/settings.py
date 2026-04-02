b9d9249d95a6982385658017ffe9e5739e84d5b3
7devshayanii <devshayanikoranne@gmail.com>
2026-04-02 13:28:03 +0530

    fixed allowed hosts for deployment

--- finance_manager/settings.py
+++ finance_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l&o%thav%@^e!pv=wom3h5iej6wcqp*)-mciydmkcfye%fxq57
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
